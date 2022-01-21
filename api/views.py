2d6716016c903866b3e1ca0c0077ebcf291687d5
sylvester <nkwontasylvester15@gmail.com>
2022-01-21 10:37:05 -0900

    second commit, views.py added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from api import serializers
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
